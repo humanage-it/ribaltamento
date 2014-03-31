5f9b4b8c3968281451d52b5a2582ba274d1c0f8f
juan-herrera-humanage <juan.herrera@humanage.it>
2014-03-31 17:46:17 +1100

    Update OfferingsNoOwnerJavaAction.java
    Used ServiceLocator as parameter to the internal command.

--- src/main/java/humanage/ribaltamento/OfferingsNoOwnerJavaAction.java
+++ src/main/java/humanage/ribaltamento/OfferingsNoOwnerJavaAction.java
@@ -1,12 +1,8 @@
 package humanage.ribaltamento;
 
-import java.io.*;
-import java.util.Date;
-
 import com.saba.exception.SabaException;
 import com.saba.locator.ServiceLocator;
 import com.saba.notify.IJavaNotificationAction;
-import com.saba.util.Debug;
 
 public class OfferingsNoOwnerJavaAction
 implements IJavaNotificationAction
@@ -14,22 +10,7 @@ implements IJavaNotificationAction
 	public void executeAction(String sourceId, String sourceDomainId, ServiceLocator locator)
 	throws SabaException
 	{
-		
-		try {
-		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("e:\\SabaWeb\\humanage.txt", true)));
-		    out.println(new Date().toString());
-		    out.close();
-		} catch (IOException e) {
-			System.out.println("HUMANAGE:  OfferingsNoOwnerJavaAction error creating flag file");
-		}
-		
-		
 		OfferingsNoOwnerCommand command = new OfferingsNoOwnerCommand();
-		Debug.trace("HUMANAGE: Start Java Action OfferingsNoOwnerJavaAction");
-		System.out.println("HUMANAGE: Start Java Action OfferingsNoOwnerJavaAction");
 		command.moveToTrash(locator);
-		Debug.trace("HUMANAGE: End Java Action OfferingsNoOwnerJavaAction");
-		System.out.println("HUMANAGE: End Java Action OfferingsNoOwnerJavaAction");
-		
 	}
 }
